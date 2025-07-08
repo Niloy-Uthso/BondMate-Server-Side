03f340fca0906d9434c9a292b752e97b39b90cba
Niloy-Uthso <niloyuthso16@gmail.com>
2025-07-08 19:27:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t3hmlwb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+const database = client.db("bondmateDB"); // Name of your DB
+    const biodataCollection = database.collection("biodata"); // Your collection
+
+
+    app.post("/biodata", async (req, res) => {
+      const biodata = req.body;
+
+      // Automatically generate biodataId
+      const last = await biodataCollection.find().sort({ biodataId: -1 }).limit(1).toArray();
+      const newId = last.length ? last[0].biodataId + 1 : 1;
+      biodata.biodataId = newId;
+
+      const result = await biodataCollection.insertOne(biodata);
+      res.send({ success: true, insertedId: result.insertedId, biodataId: newId });
+    });
+
+      app.get("/biodata", async (req, res) => {
+      const biodatas = await biodataCollection.find().toArray();
+      res.send(biodatas);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Matrimony server is running!");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
